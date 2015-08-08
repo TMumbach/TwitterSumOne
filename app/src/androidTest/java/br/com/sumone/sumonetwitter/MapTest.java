92661d9c190529f7f8d2bb2008ed1460c12a1143
TMumbach <tiagomumbach@gmail.com>
2015-08-08 20:05:35 -0300

    Removido bibloteca não utilizada

---
+++ app/src/androidTest/java/br/com/sumone/sumonetwitter/MapTest.java
@@ -0,0 +1,49 @@
+package br.com.sumone.sumonetwitter;
+
+import android.content.Context;
+import android.test.ServiceTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import junit.framework.TestCase;
+
+import java.lang.reflect.Method;
+
+import br.com.sumone.sumonetwitter.map.TweetMapFragment;
+
+/**
+ * Created by Tiago on 08/08/2015.
+ */
+public class MapTest extends TestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @SmallTest
+    public void locationByAddress() {
+        LatLng locationFromAddress = TweetMapFragment.getLocationFromAddress(getTestContext(), "São José, Santa catarina - Brasil");
+        assertNotNull(locationFromAddress);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    private Context getTestContext()
+    {
+        try
+        {
+            Method getTestContext = ServiceTestCase.class.getMethod("getTestContext");
+            return (Context) getTestContext.invoke(this);
+        }
+        catch (final Exception exception)
+        {
+            exception.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ app/src/androidTest/java/br/com/sumone/sumonetwitter/ViewInjectTest.java
@@ -0,0 +1,9 @@
+package br.com.sumone.sumonetwitter;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Tiago on 08/08/2015.
+ */
+public class ViewInjectTest extends TestCase {
+}
